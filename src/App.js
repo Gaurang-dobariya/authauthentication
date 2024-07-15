7019a49dc7f97a05fea726f670f722631727dbbb
Gaurang-dobariya <gaurangdobariya870@gmail.com>
2024-07-15 14:53:00 +0530

    auth0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "./components/profile";
 
 function App() {
+
+
+  let { loginWithRedirect, logout } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile />
+      <button onClick={() => loginWithRedirect()}>Login</button>
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Log Out
+      </button>
+    </>
   );
 }
 
